d7959d748668d3f21344b0bb179f68184c315c43
student <student@softeng>
2017-02-21 01:09:35 -0800

    finished

---
+++ TicTacToeBoardTest.cpp
@@ -0,0 +1,20 @@
+/**
+ * Unit Tests for TicTacToeBoard
+**/
+
+#include <gtest/gtest.h>
+#include "TicTacToeBoard.h"
+ 
+class TicTacToeBoardTest : public ::testing::Test
+{
+	protected:
+		TicTacToeBoardTest(){} //constructor runs before each test
+		virtual ~TicTacToeBoardTest(){} //destructor cleans up after tests
+		virtual void SetUp(){} //sets up before each test (after constructor)
+		virtual void TearDown(){} //clean up after each test, (before destructor) 
+};
+
+TEST(TicTacToeBoardTest, sanityCheck)
+{
+	ASSERT_TRUE(true);
+}
